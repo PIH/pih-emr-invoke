b4cdbba50130d202c04c4bdb23fcc05316340181
Brandon Istenes <bistenes@gmail.com>
2018-10-08 16:50:08 -0500

    metadata-bundles/update.sh should delete dupe packages

--- metadata-bundles/update.sh
+++ metadata-bundles/update.sh
@@ -1,2 +1,6 @@
 #!/usr/env/bin bash
 unzip -B '../openmrs-module-mirebalaismetadata/api/src/main/resources/*.zip'
+for i in $(seq 27 54); do rm header.xml~$i metadata.xml~$i ; done
+
+echo "Just deleted packages 27-54, assuming there's 27 MDS packages."
+echo "If the number of MDS packages has changed, you'll need to change this script."
